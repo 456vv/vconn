8e842318563ef17c00a0f3cabb82fbe87443e806
456vv <123@456vv.com>
2021-05-30 10:01:46 +0800

    1，支持限制读取 2，支持禁止后台读取 3，支持多线程调用CloseNotify监听关闭状态

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ type CloseNotifier interface {                          // 关闭通知接口
 type Conn struct {                                      // 连接
 }
     func NewConn(c net.Conn) net.Conn                           // 新连接
-    func (T *Conn) SetReadDiscard(ok bool)                      // 读取读取丢弃
+    func (T *Conn) SetReadDiscard(y bool)                       // 读取读取丢弃
+    func (T *Conn) SetReadLimit(remain int)                     // 限制读取大小
+    func (T *Conn) DisableBackgroundRead(y bool)                // 禁止后台读取
     func (T *Conn) CloseNotify() <-chan error                   // 关闭通知
     func (T *Conn) Read(b []byte) (n int, err error)            // 读取
     func (T *Conn) Write(b []byte) (n int, err error)           // 写入
--- conn.go
+++ conn.go
@@ -7,6 +7,7 @@ import(
 	"os"
 	"sync/atomic"
 	"io"
+	"math"
 )
 type atomicBool int32
 func (T *atomicBool) isTrue() bool 	{ return atomic.LoadInt32((*int32)(T)) != 0 }
@@ -20,56 +21,76 @@ type CloseNotifier interface {
 
 type Conn struct {
 	rwc						net.Conn
-	closedSignal 			chan error
+	closeErr				error
+	closeSignal				[]chan error
 	r						*connReader
 	readDeadline			time.Time
 	writeDeadline			time.Time
 	m						sync.Mutex
 	closed					atomicBool
-	receiveNotifying		atomicBool
+	disableBackgroundRead	bool
 	backgroundReadDiscard	bool
 }
 func NewConn(c net.Conn) net.Conn {
 	if conn, ok := c.(*Conn); ok {
 		return conn
 	}
-	conn := &Conn{rwc:c, closedSignal:make(chan error, 1)}
+	conn := &Conn{rwc:c}
 	conn.r = &connReader{conn:conn}
 	return conn
 }
 
 //后台读取丢弃，只要用于连接加入连接池后，期间收到的数据全部丢弃。
 //用于特殊环境，普通用户正常不需要用到他。
-func (T *Conn) SetBackgroundReadDiscard(ok bool) {
-	T.backgroundReadDiscard = ok
+func (T *Conn) SetBackgroundReadDiscard(y bool) {
+	T.backgroundReadDiscard = y
+}
+
+//设置读取限制
+//	remain int	设置 0 默认限制为(math.MaxInt32)
+func (T *Conn) SetReadLimit(remain int) {
+	T.r.setReadLimit(remain)
+}
+
+//停止后台实时监听的连接关闭状态，在你自己调用Read或Write发生错误，依然 CloseNotify 返回连接关闭信号。
+func (T *Conn) DisableBackgroundRead(y bool){
+	T.disableBackgroundRead=y
+	if y {
+		//中止后台读取
+		T.r.abortBackgroundRead()
+	}
 }
 
 //注意：这里会有两个通知，1）远程主动断开 2）本地调用断开
 //如果你是用于断开连接重连，需要判断返回的 error 状态。
 //error != nil 表示远程主动断开（一般用于这个）
 //error == nil 表示本地调用断开（关闭连接，通道关闭，返回nil）
-func (T *Conn) CloseNotify() <-chan error {	
-	if T.closed.isFalse() {
-		T.receiveNotifying.setTrue()
-		T.r.startBackgroundRead()
+func (T *Conn) CloseNotify() <-chan error {
+	T.m.Lock()
+	defer T.m.Unlock()
+	
+	c := make(chan error, 1)
+	if T.closeErr != nil {
+		c<- T.closeErr
+		return c
 	}
-	return T.closedSignal
+	
+	T.r.startBackgroundRead()
+	T.closeSignal = append(T.closeSignal, c)
+	return c
 }
 func (T *Conn) closeNotify(err error) {
 	T.m.Lock()
 	defer T.m.Unlock()
 	
 	if isCommonNetError(err) {
-		select{
-		case _, ok := <-T.closedSignal:
-			if !ok {
-				return
-			}
-		default:
-		}
 		
-		T.receiveNotifying.setFalse()
-		T.closedSignal <- err
+		for _, c  := range T.closeSignal {
+			c <- err
+			close(c)
+		}
+		T.closeSignal=nil
+		T.closeErr = err
 	}
 }
 func (T *Conn) Read(b []byte) (n int, err error) {
@@ -77,7 +98,7 @@ func (T *Conn) Read(b []byte) (n int, err error) {
 	T.closeNotify(err)
 	//仅限在用户主动读取的时候，并之前没有收到通知事件情况下才能再次开启后台监听
 	//因为用户主动读取时候关闭了后台监听
-	if T.closed.isFalse() && T.receiveNotifying.isTrue() {
+	if T.closeErr == nil && len(T.closeSignal) != 0 {
 		T.r.startBackgroundRead()
 	}
 	return
@@ -91,18 +112,12 @@ func (T *Conn) Close() error {
 	T.m.Lock()
 	defer T.m.Unlock()
 	
-	if T.closed.setTrue() {
-		return nil
+	for _, c  := range T.closeSignal {
+		close(c)
 	}
+	T.closeSignal=nil
+	T.closeErr = io.EOF
 	
-	select{
-	case _, ok := <-T.closedSignal:
-		if ok {
-			close(T.closedSignal)
-		}
-	default:
-		close(T.closedSignal)
-	}
 	return T.rwc.Close()
 }
 func (T *Conn) LocalAddr() net.Addr {
@@ -132,8 +147,11 @@ type connReader struct {
   	hasByte atomicBool														// 检测有数据
   	byteBuf [1]byte															// 第一个数据，检测时候得到一个数据
   	cond    *sync.Cond														// 组
-  	inRead  atomicBool														// 正在读取
-  	aborted atomicBool														// 结束
+  	inRead  	atomicBool													// 正在读取
+  	aborted 	atomicBool													// 结束
+  	inBackRead 	atomicBool													// 当前是在后台读取
+  	remain	int																// 限制读取
+  	readLimit bool
 }
 
 //锁，条件等待
@@ -147,9 +165,29 @@ func (T *connReader) lock() {
 //解锁
 func (T *connReader) unlock() {T.mu.Unlock()}
 
+//设置读取限制
+//	remain int	设置 0 默认限制为(math.MaxInt32)
+func (T *connReader) setReadLimit(remain int) {
+	if(remain == 0){
+		T.remain = math.MaxInt32
+		T.readLimit=false
+		return
+	}
+	T.remain = remain
+	T.readLimit=true
+}
+
+//超出读取限制
+func (T *connReader) hitReadLimit() bool {
+	if(!T.readLimit){
+		return false
+	}
+	return T.remain <= 0
+}
+
 //开始后台读取
 func (T *connReader) startBackgroundRead() {
-  	if T.inRead.isTrue() {
+  	if T.inRead.isTrue() || T.conn.disableBackgroundRead {
   		return
   	}
   	if T.hasByte.isTrue() {
@@ -161,23 +199,28 @@ func (T *connReader) startBackgroundRead() {
 //后台读取
 func (T *connReader) backgroundRead() {
 	T.lock()
-	if T.inRead.setTrue() {
+	if T.conn.disableBackgroundRead {
 		T.unlock()
 		return
 	}
+	if T.inRead.setTrue() { //这里是设置，并返回原值
+		T.unlock()
+		return
+	}
+	T.inBackRead.setTrue()
 	T.unlock()
   	var n int
   	var err error
   	if T.conn.backgroundReadDiscard {
   		buf := make([]byte, 512)
-  		for {
+  		for T.aborted.isFalse() {//多线程，已经中止后，防止依然执行。
   			T.conn.rwc.SetReadDeadline(time.Time{})
   			_, err = T.conn.rwc.Read(buf)
   			if err != nil {
   				break
   			}
   		}
-  	}else{
+  	}else if T.aborted.isFalse() {//多线程，已经中止后，防止依然执行。
   		T.conn.rwc.SetReadDeadline(time.Time{})
 		n, err = T.conn.rwc.Read(T.byteBuf[:])
   	}
@@ -193,11 +236,21 @@ func (T *connReader) backgroundRead() {
 	}
 	T.aborted.setFalse()
 	T.inRead.setFalse()
+	T.inBackRead.setFalse()
 	T.unlock()
 	T.cond.Broadcast()
 }
-
-//中止等待读取
+//中止后台读取
+func (T *connReader) abortBackgroundRead(){
+	T.lock()
+	if T.inBackRead.isTrue() {
+		T.unlock()
+		T.abortPendingRead()
+		return
+	}
+	T.unlock()
+}
+//中止正在读取
 func (T *connReader) abortPendingRead() {
 	T.lock()
 	defer T.unlock()
@@ -225,6 +278,15 @@ func (T *connReader) Read(p []byte) (n int, err error) {
 		T.lock()
 	}
 	
+	if T.hitReadLimit() {
+		T.unlock()
+		return 0, io.EOF
+	}
+	
+	if T.readLimit && len(p) > T.remain {
+		p = p[:T.remain]
+	}
+	
 	pt := p
 	var hasByte = T.hasByte.isTrue()
 	if hasByte {
@@ -232,6 +294,7 @@ func (T *connReader) Read(p []byte) (n int, err error) {
 		T.hasByte.setFalse()
 		if len(p) == 1 {
 			T.unlock()
+			T.remain --
 			return 1, nil
 		}
 		pt = p[1:]
@@ -244,6 +307,9 @@ func (T *connReader) Read(p []byte) (n int, err error) {
 	if hasByte {
 		n++
 		err = nil
+	}
+	if T.readLimit {
+		T.remain -= n
 	}
 	return n, err
 }
